8e2e5eb41b87a0c9049ec2c56f63a2a8252841b0
divine-mercyx <divinewilson766@gmail.com>
2025-06-28 14:33:45 +0100

    safe zone website

--- safezone-website.tsx
+++ safezone-website.tsx
@@ -51,7 +51,7 @@ export default function SafeZoneWebsite() {
             {/*  <Shield className="w-5 h-5 mr-2" />*/}
             {/*  Create a Report*/}
             {/*</Button>*/}
-            <a href="http://localhost:5173/" className="bg-blue-600 flex flex-col sm:flex-row gap-4 justify-center p-2 text-white rounded text-decoration-none hover:bg-blue-700">
+            <a href="http://localhost:5173/" className="bg-blue-600 flex flex-col sm:flex-row gap-2 justify-center p-2 text-white rounded text-decoration-none hover:bg-blue-700">
               <Shield className="w-5 h-5 mr-2" />
               Create a Report
             </a>
